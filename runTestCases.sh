f8dcdf7a8ba19f47d8f5eb9a1e906d4b123d6f27
Martyn Clark <mclark@ucar.edu>
2015-11-27 23:19:05 +0000

    overhaul of runTestCases.sh to run on multiple processors

--- runTestCases.sh
+++ runTestCases.sh
@@ -12,11 +12,14 @@
 # =================================================================================================
 # User-configurable component
 
-# Define the experiment (e.g., the name of the current branch)
-expName=_master
+# Define the number of processors
+nProcessors=20
 
-# Set the path to the SUMMA executable (e.g. /usr/local/bin/ or wherever you have installed SUMMA)
-summaPath=/Users/mclark/check/upstream/summa/bin
+# Define the summa instance (core directory where summa is installed)
+summaPath=/home/mclark/check/upstream/summa
+
+# Define the experiment (e.g., the name of the current branch)
+expName=develop
 
 # end of user-configuable component
 # =================================================================================================
@@ -27,95 +30,184 @@ if  [ -z ${summaPath} ]; then
  exit 1
 fi
 
+# define the current directory
+currentDir=`pwd`
+
+# change directory to the summa installation and check out the desired branch
+cd $summaPath/build
+git checkout $expName
+
+# compile
+make 2> make.log
+
+# change back to the current directory
+cd $currentDir
+
 # define new settings and output directories
-settingsNew='settings'${expName}
-outputNew='output'${expName}
+settingsNew='settings_'${expName}
+outputNew='output_'${expName}
 
 # Define the summa executable
-SUMMA_EXE=${summaPath}/summa${expName}.exe
+SUMMA_EXE=${summaPath}/bin/summa_${expName}.exe
 
 # Copy the executable
-cp ${summaPath}/summa.exe ${SUMMA_EXE}
+cp ${summaPath}/bin/summa.exe ${SUMMA_EXE}
 
-# Configure control files and directories for the current experiment
-./modifyTestCases.sh $expName
+# create a new copy of the settings directory
+mkdir -p $settingsNew
+cp -rp settings/* $settingsNew
+
+# make the output directory
+mkdir -p $outputNew
+
+# create a directory for some log files
+mkdir -p ${outputNew}/log
+
+# create the paths for the output files
+mkdir -p ${outputNew}/syntheticTestCases/celia1990
+mkdir -p ${outputNew}/syntheticTestCases/miller1998
+mkdir -p ${outputNew}/syntheticTestCases/mizoguchi1990
+mkdir -p ${outputNew}/syntheticTestCases/wigmosta1999
+mkdir -p ${outputNew}/wrrPaperTestCases/figure01
+mkdir -p ${outputNew}/wrrPaperTestCases/figure02
+mkdir -p ${outputNew}/wrrPaperTestCases/figure03
+mkdir -p ${outputNew}/wrrPaperTestCases/figure04
+mkdir -p ${outputNew}/wrrPaperTestCases/figure05
+mkdir -p ${outputNew}/wrrPaperTestCases/figure06
+mkdir -p ${outputNew}/wrrPaperTestCases/figure07
+mkdir -p ${outputNew}/wrrPaperTestCases/figure08
+mkdir -p ${outputNew}/wrrPaperTestCases/figure09
+
+# modify the paths in the settings files
+for file in `grep -l '/output/' -R ${settingsNew}`; do
+ sed "s|/settings/|/${settingsNew}/|" $file > junk
+ sed "s|/output/|/${outputNew}/|" junk > $file
+ rm junk
+done
+
+# *************************************************************************************************
+# *************************************************************************************************
+# *************************************************************************************************
+# define a function to call summa
+runSumma ()
+{	
+
+# define what the arguments mean
+exeName=$1         # name of executable
+uniqueID=$2        # unique identifier
+runName=$3         # name of experiment
+fileManager=$4     # name of the fileManager file
+logFile=$5         # name of the log file
+
+echo '* running experiment ' $uniqueID
+
+# make a control file
+ctlFile=summa.${uniqueID}.control
+touch $ctlFile
+
+# run the model
+$exeName $runName $fileManager > $logFile
+
+# remove the control file
+rm $ctlFile
+
+}
 
 # *************************************************************************************************
 # * PART 1: TEST CASES BASED ON SYNTHETIC OR LAB DATA
 
-# Synthetic test case 1: Simulations from Celia (WRR 1990)
-${SUMMA_EXE} _testSumma ${settingsNew}/syntheticTestCases/celia1990/summa_fileManager_celia1990.txt > ${outputNew}/log/a1.log
-echo ${outputNew} 'completed synthetic test case 1'
+# loop through experiments
+for ix in `seq -w 1 7`; do
 
-# Synthetic test case 2: Simulations from Miller (WRR 1998)
-${SUMMA_EXE} _testSumma ${settingsNew}/syntheticTestCases/miller1998/summa_fileManager_millerClay.txt > ${outputNew}/log/a2a.log
-${SUMMA_EXE} _testSumma ${settingsNew}/syntheticTestCases/miller1998/summa_fileManager_millerLoam.txt > ${outputNew}/log/a2b.log
-${SUMMA_EXE} _testSumma ${settingsNew}/syntheticTestCases/miller1998/summa_fileManager_millerSand.txt > ${outputNew}/log/a2c.log
-echo ${outputNew} 'completed synthetic test case 2'
+ # define experiment name
+ exp=s${ix}
 
-# Synthetic test case 3: Simulations of the lab experiment of Mizoguchi (1990)
-#                         as described by Hansson et al. (VZJ 2005)
-${SUMMA_EXE} _testSumma ${settingsNew}/syntheticTestCases/mizoguchi1990/summa_fileManager_mizoguchi.txt > ${outputNew}/log/a3.log
-echo ${outputNew} 'completed synthetic test case 3'
+ # Synthetic test case 1: Simulations from Celia (WRR 1990)
+ if [ "$ix" = 1 ]; then runSumma ${SUMMA_EXE} ${exp} _testSumma ${settingsNew}/syntheticTestCases/celia1990/summa_fileManager_celia1990.txt ${outputNew}/log/${exp}.log & fi
 
-# Synthetic test case 4: Simulations of rain on a sloping hillslope from Wigmosta (WRR 1999)
-${SUMMA_EXE} _testSumma ${settingsNew}/syntheticTestCases/wigmosta1999/summa_fileManager-exp1.txt > ${outputNew}/log/a4a.log
-${SUMMA_EXE} _testSumma ${settingsNew}/syntheticTestCases/wigmosta1999/summa_fileManager-exp2.txt > ${outputNew}/log/a4b.log
-echo ${outputNew} 'completed synthetic test case 4'
+ # Synthetic test case 2: Simulations from Miller (WRR 1998)
+ if [ "$ix" = 2 ] ; then runSumma ${SUMMA_EXE} ${exp} _testSumma ${settingsNew}/syntheticTestCases/miller1998/summa_fileManager_millerClay.txt ${outputNew}/log/${exp}a.log & fi
+ if [ "$ix" = 3 ] ; then runSumma ${SUMMA_EXE} ${exp} _testSumma ${settingsNew}/syntheticTestCases/miller1998/summa_fileManager_millerLoam.txt ${outputNew}/log/${exp}b.log & fi
+ if [ "$ix" = 4 ] ; then runSumma ${SUMMA_EXE} ${exp} _testSumma ${settingsNew}/syntheticTestCases/miller1998/summa_fileManager_millerSand.txt ${outputNew}/log/${exp}c.log & fi
 
-# End of test cases based on synthetic/lab data
+ # Synthetic test case 3: Simulations of the lab experiment of Mizoguchi (1990) as described by Hansson et al. (VZJ 2005)
+ if [ "$ix" = 5 ] ; then runSumma ${SUMMA_EXE} ${exp} _testSumma ${settingsNew}/syntheticTestCases/mizoguchi1990/summa_fileManager_mizoguchi.txt ${outputNew}/log/${exp}.log & fi
+
+ # Synthetic test case 4: Simulations of rain on a sloping hillslope from Wigmosta (WRR 1999)
+ if [ "$ix" = 6 ]; then runSumma ${SUMMA_EXE} ${exp} _testSumma ${settingsNew}/syntheticTestCases/wigmosta1999/summa_fileManager-exp1.txt ${outputNew}/log/${exp}a.log & fi
+ if [ "$ix" = 7 ]; then runSumma ${SUMMA_EXE} ${exp} _testSumma ${settingsNew}/syntheticTestCases/wigmosta1999/summa_fileManager-exp2.txt ${outputNew}/log/${exp}b.log & fi
+
+ # sleep awhile, to ensure not rapidly executing programs before we realized it
+ sleep 1
+
+ # sleep if using the desired number of processors
+ while [ `ls -1 summa.*.control | wc -l` -ge $nProcessors ]; do
+  sleep 5
+ done  # check if using the number of processors
+
+done  # End of test cases based on synthetic/lab data
+# *************************************************************************************************
+
+
 # *************************************************************************************************
 # * PART 2: TEST CASES BASED ON FIELD DATA, AS DESCRIBED BY CLARK ET AL. (WRR 2015B)
 
-# Figure 1: Radiation transmission through an Aspen stand, Reynolds Mountain East
-${SUMMA_EXE} _riparianAspenBeersLaw        ${settingsNew}/wrrPaperTestCases/figure01/summa_fileManager_riparianAspenBeersLaw.txt  > ${outputNew}/log/b1a.log
-${SUMMA_EXE} _riparianAspenNLscatter       ${settingsNew}/wrrPaperTestCases/figure01/summa_fileManager_riparianAspenNLscatter.txt > ${outputNew}/log/b1b.log
-${SUMMA_EXE} _riparianAspenUEB2stream      ${settingsNew}/wrrPaperTestCases/figure01/summa_fileManager_riparianAspenUEB2stream.txt > ${outputNew}/log/b1c.log
-${SUMMA_EXE} _riparianAspenCLM2stream      ${settingsNew}/wrrPaperTestCases/figure01/summa_fileManager_riparianAspenCLM2stream.txt > ${outputNew}/log/b1d.log
-${SUMMA_EXE} _riparianAspenVegParamPerturb ${settingsNew}/wrrPaperTestCases/figure01/summa_fileManager_riparianAspenVegParamPerturb.txt > ${outputNew}/log/b1e.log
-echo ${outputNew} 'completed field test case 1'
+# loop through experiments
+for ix in `seq -w 1 22`; do
 
-# Figure 2: Wind attenuation through an Aspen stand, Reynolds Mountain East
-${SUMMA_EXE} _riparianAspenWindParamPerturb ${settingsNew}/wrrPaperTestCases/figure02/summa_fileManager_riparianAspenWindParamPerturb.txt > ${outputNew}/log/b2.log
-echo ${outputNew} 'completed field test case 2'
+ # define experiment name
+ exp=f${ix}
 
-# Figure 3: Impacts of canopy wind profile on surface fluxes, surface temperature, and snow melt (Aspen stand, Reynolds Mountain East)
-${SUMMA_EXE} _riparianAspenExpWindProfile ${settingsNew}/wrrPaperTestCases/figure03/summa_fileManager_riparianAspenExpWindProfile.txt > ${outputNew}/log/b3.log
-echo ${outputNew} 'completed field test case 3'
+ # Figure 1: Radiation transmission through an Aspen stand, Reynolds Mountain East
+ if [ "$ix" = 01 ]; then runSumma ${SUMMA_EXE} ${exp} _riparianAspenBeersLaw        ${settingsNew}/wrrPaperTestCases/figure01/summa_fileManager_riparianAspenBeersLaw.txt  ${outputNew}/log/${exp}a.log & fi
+ if [ "$ix" = 02 ]; then runSumma ${SUMMA_EXE} ${exp} _riparianAspenNLscatter       ${settingsNew}/wrrPaperTestCases/figure01/summa_fileManager_riparianAspenNLscatter.txt ${outputNew}/log/${exp}b.log & fi
+ if [ "$ix" = 03 ]; then runSumma ${SUMMA_EXE} ${exp} _riparianAspenUEB2stream      ${settingsNew}/wrrPaperTestCases/figure01/summa_fileManager_riparianAspenUEB2stream.txt ${outputNew}/log/${exp}c.log & fi
+ if [ "$ix" = 04 ]; then runSumma ${SUMMA_EXE} ${exp} _riparianAspenCLM2stream      ${settingsNew}/wrrPaperTestCases/figure01/summa_fileManager_riparianAspenCLM2stream.txt ${outputNew}/log/${exp}d.log & fi
+ if [ "$ix" = 05 ]; then runSumma ${SUMMA_EXE} ${exp} _riparianAspenVegParamPerturb ${settingsNew}/wrrPaperTestCases/figure01/summa_fileManager_riparianAspenVegParamPerturb.txt ${outputNew}/log/${exp}e.log & fi
 
-# Figure 4: Form of different interception capacity parameterizations
-# (no model simulations conducted/needed)
+ # Figure 2: Wind attenuation through an Aspen stand, Reynolds Mountain East
+ if [ "$ix" = 06 ]; then runSumma ${SUMMA_EXE} ${exp} _riparianAspenWindParamPerturb ${settingsNew}/wrrPaperTestCases/figure02/summa_fileManager_riparianAspenWindParamPerturb.txt ${outputNew}/log/${exp}.log & fi
 
-# Figure 5: Snow interception at Umpqua
-${SUMMA_EXE} _hedpom9697 ${settingsNew}/wrrPaperTestCases/figure05/summa_fileManager_9697_hedpom.txt > ${outputNew}/log/b5a.log
-${SUMMA_EXE} _hedpom9798 ${settingsNew}/wrrPaperTestCases/figure05/summa_fileManager_9798_hedpom.txt > ${outputNew}/log/b5b.log
-${SUMMA_EXE} _storck9697 ${settingsNew}/wrrPaperTestCases/figure05/summa_fileManager_9697_storck.txt > ${outputNew}/log/b5c.log
-${SUMMA_EXE} _storck9798 ${settingsNew}/wrrPaperTestCases/figure05/summa_fileManager_9798_storck.txt > ${outputNew}/log/b5d.log
-echo ${outputNew} 'completed field test case 5'
+ # Figure 3: Impacts of canopy wind profile on surface fluxes, surface temperature, and snow melt (Aspen stand, Reynolds Mountain East)
+ if [ "$ix" = 07 ]; then runSumma ${SUMMA_EXE} ${exp} _riparianAspenExpWindProfile ${settingsNew}/wrrPaperTestCases/figure03/summa_fileManager_riparianAspenExpWindProfile.txt ${outputNew}/log/${exp}.log & fi
 
-# Figure 6: Sensitivity to snow albedo representations at Reynolds Mountain East and Senator Beck
-${SUMMA_EXE} _reynoldsConstantDecayRate ${settingsNew}/wrrPaperTestCases/figure06/summa_fileManager_reynoldsConstantDecayRate.txt > ${outputNew}/log/b6a.log
-${SUMMA_EXE} _reynoldsVariableDecayRate ${settingsNew}/wrrPaperTestCases/figure06/summa_fileManager_reynoldsVariableDecayRate.txt > ${outputNew}/log/b6b.log
-${SUMMA_EXE} _senatorConstantDecayRate  ${settingsNew}/wrrPaperTestCases/figure06/summa_fileManager_senatorConstantDecayRate.txt > ${outputNew}/log/b6c.log
-${SUMMA_EXE} _senatorVariableDecayRate  ${settingsNew}/wrrPaperTestCases/figure06/summa_fileManager_senatorVariableDecayRate.txt > ${outputNew}/log/b6d.log
-echo ${outputNew} 'completed field test case 6'
+ # Figure 4: Form of different interception capacity parameterizations
+ # (no model simulations conducted/needed)
 
-# Figure 7: Sensitivity of ET to the stomatal resistance parameterization (Aspen stand at Reynolds Mountain East)
-${SUMMA_EXE} _jarvis           ${settingsNew}/wrrPaperTestCases/figure07/summa_fileManager_riparianAspenJarvis.txt > ${outputNew}/log/b7a.log
-${SUMMA_EXE} _ballBerry        ${settingsNew}/wrrPaperTestCases/figure07/summa_fileManager_riparianAspenBallBerry.txt > ${outputNew}/log/b7b.log
-${SUMMA_EXE} _simpleResistance ${settingsNew}/wrrPaperTestCases/figure07/summa_fileManager_riparianAspenSimpleResistance.txt > ${outputNew}/log/b7c.log
-echo ${outputNew} 'completed field test case 7'
+ # Figure 5: Snow interception at Umpqua
+ if [ "$ix" = 08 ]; then runSumma ${SUMMA_EXE} ${exp} _hedpom9697 ${settingsNew}/wrrPaperTestCases/figure05/summa_fileManager_9697_hedpom.txt ${outputNew}/log/${exp}a.log & fi
+ if [ "$ix" = 09 ]; then runSumma ${SUMMA_EXE} ${exp} _hedpom9798 ${settingsNew}/wrrPaperTestCases/figure05/summa_fileManager_9798_hedpom.txt ${outputNew}/log/${exp}b.log & fi
+ if [ "$ix" = 10 ]; then runSumma ${SUMMA_EXE} ${exp} _storck9697 ${settingsNew}/wrrPaperTestCases/figure05/summa_fileManager_9697_storck.txt ${outputNew}/log/${exp}c.log & fi
+ if [ "$ix" = 11 ]; then runSumma ${SUMMA_EXE} ${exp} _storck9798 ${settingsNew}/wrrPaperTestCases/figure05/summa_fileManager_9798_storck.txt ${outputNew}/log/${exp}d.log & fi
 
-# Figure 8: Sensitivity of ET to the root distribution and the baseflow parameterization (Aspen stand at Reynolds Mountain East)
-#  (NOTE: baseflow simulations conducted as part of Figure 9)
-${SUMMA_EXE} _perturbRoots ${settingsNew}/wrrPaperTestCases/figure08/summa_fileManager_riparianAspenPerturbRoots.txt > ${outputNew}/log/b8.log
-echo ${outputNew} 'completed field test case 8'
+ # Figure 6: Sensitivity to snow albedo representations at Reynolds Mountain East and Senator Beck
+ if [ "$ix" = 12 ]; then runSumma ${SUMMA_EXE} ${exp} _reynoldsConstantDecayRate ${settingsNew}/wrrPaperTestCases/figure06/summa_fileManager_reynoldsConstantDecayRate.txt ${outputNew}/log/${exp}a.log & fi
+ if [ "$ix" = 13 ]; then runSumma ${SUMMA_EXE} ${exp} _reynoldsVariableDecayRate ${settingsNew}/wrrPaperTestCases/figure06/summa_fileManager_reynoldsVariableDecayRate.txt ${outputNew}/log/${exp}b.log & fi
+ if [ "$ix" = 14 ]; then runSumma ${SUMMA_EXE} ${exp} _senatorConstantDecayRate  ${settingsNew}/wrrPaperTestCases/figure06/summa_fileManager_senatorConstantDecayRate.txt ${outputNew}/log/${exp}c.log & fi
+ if [ "$ix" = 15 ]; then runSumma ${SUMMA_EXE} ${exp} _senatorVariableDecayRate  ${settingsNew}/wrrPaperTestCases/figure06/summa_fileManager_senatorVariableDecayRate.txt ${outputNew}/log/${exp}d.log & fi
 
-# Figure 9: Simulations of runoff using different baseflow parameterizations (Reynolds Mountain East)
-${SUMMA_EXE} _1dRichards          ${settingsNew}/wrrPaperTestCases/figure09/summa_fileManager_1dRichards.txt > ${outputNew}/log/b9a.log
-${SUMMA_EXE} _lumpedTopmodel      ${settingsNew}/wrrPaperTestCases/figure09/summa_fileManager_lumpedTopmodel.txt > ${outputNew}/log/b9b.log
-${SUMMA_EXE} _distributedTopmodel ${settingsNew}/wrrPaperTestCases/figure09/summa_fileManager_distributedTopmodel.txt > ${outputNew}/log/b9c.log
-echo ${outputNew} 'completed field test case 9'
+ # Figure 7: Sensitivity of ET to the stomatal resistance parameterization (Aspen stand at Reynolds Mountain East)
+ if [ "$ix" = 16 ]; then runSumma ${SUMMA_EXE} ${exp} _jarvis           ${settingsNew}/wrrPaperTestCases/figure07/summa_fileManager_riparianAspenJarvis.txt ${outputNew}/log/${exp}a.log & fi
+ if [ "$ix" = 17 ]; then runSumma ${SUMMA_EXE} ${exp} _ballBerry        ${settingsNew}/wrrPaperTestCases/figure07/summa_fileManager_riparianAspenBallBerry.txt ${outputNew}/log/${exp}b.log & fi
+ if [ "$ix" = 18 ]; then runSumma ${SUMMA_EXE} ${exp} _simpleResistance ${settingsNew}/wrrPaperTestCases/figure07/summa_fileManager_riparianAspenSimpleResistance.txt ${outputNew}/log/${exp}c.log & fi
 
-# End of test cases based on field data
+ # Figure 8: Sensitivity of ET to the root distribution and the baseflow parameterization (Aspen stand at Reynolds Mountain East)
+ #  (NOTE: baseflow simulations conducted as part of Figure 9)
+ if [ "$ix" = 19 ]; then runSumma ${SUMMA_EXE} ${exp} _perturbRoots ${settingsNew}/wrrPaperTestCases/figure08/summa_fileManager_riparianAspenPerturbRoots.txt ${outputNew}/log/${exp}.log & fi
+
+ # Figure 9: Simulations of runoff using different baseflow parameterizations (Reynolds Mountain East)
+ if [ "$ix" = 20 ]; then runSumma ${SUMMA_EXE} ${exp} _1dRichards          ${settingsNew}/wrrPaperTestCases/figure09/summa_fileManager_1dRichards.txt ${outputNew}/log/${exp}a.log & fi
+ if [ "$ix" = 21 ]; then runSumma ${SUMMA_EXE} ${exp} _lumpedTopmodel      ${settingsNew}/wrrPaperTestCases/figure09/summa_fileManager_lumpedTopmodel.txt ${outputNew}/log/${exp}b.log & fi
+ if [ "$ix" = 22 ]; then runSumma ${SUMMA_EXE} ${exp} _distributedTopmodel ${settingsNew}/wrrPaperTestCases/figure09/summa_fileManager_distributedTopmodel.txt ${outputNew}/log/${exp}c.log& fi
+
+ # sleep awhile, to ensure not rapidly executing programs before we realized it
+ sleep 1
+
+ # sleep if using the desired number of processors
+ while [ `ls -1 summa.*.control | wc -l` -ge $nProcessors ]; do
+  sleep 5
+ done  # check if using the number of processors
+
+done # End of test cases based on field data
 # *************************************************************************************************
+
