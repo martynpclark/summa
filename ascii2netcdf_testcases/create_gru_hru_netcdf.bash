de9dda0e252ae4cbd63c526e6141628adad47190
Andy Newman <anewman@ucar.edu>
2015-12-05 06:29:28 +0000

    Changes to create_gru_hru_netcdf.bash

--- ascii2netcdf_testcases/create_gru_hru_netcdf.bash
+++ ascii2netcdf_testcases/create_gru_hru_netcdf.bash
@@ -20,41 +20,49 @@ fi
 cdl_path=$1
 
 
-for cases in syntheticTestCases #wrrPaperTestCases
+for cases in wrrPaperTestCases #syntheticTestCases #
 do
   cd ${cases}
 
   for example in *
-#  for example in wi*
+#  for example in figure01
   do
     echo ${cases}/${example}
 
     cd ${example}
      
-    #create list of hrus
-    awk '{if(substr($1,1,1) != "!" && substr($1,1,1) != "h") print $1}' < summa_zLocalAttributes.txt > hru_list.txt
-    num_hru=`cat hru_list.txt | wc -l`
+    
+    for att in summa_zLocalAttributes*.txt
+    do 
+      token="${att#summa_zLocalAttributes_}"
+      runtype="${token%.txt}"
+      
 
-    #copy base cdl file and modify as necessary for hru dimension
-    cp ${cdl_path}/meta_gru_hru_map.cdl .
-    awk '{nh="'${num_hru}'"; if($1=="hru_dim"){printf("        hru_dim = %s ;\n",nh)} else {print $0}}' < meta_gru_hru_map.cdl > t.txt
-    mv t.txt meta_gru_hru_map.cdl
+      #create list of hrus
+      awk '{if(substr($1,1,1) != "!" && substr($1,1,1) != "h") print $1}' < ${att} > hru_list.txt
+      num_hru=`cat hru_list.txt | wc -l`
 
-    #make netcdf file quick
-    ncgen ./meta_gru_hru_map.cdl -o ${example}_gru_hru_map.nc
+      #copy base cdl file and modify as necessary for hru dimension
+      cp ${cdl_path}/meta_gru_hru_map.cdl .
+      awk '{nh="'${num_hru}'"; if($1=="hru_dim"){printf("        hru_dim = %s ;\n",nh)} else {print $0}}' < meta_gru_hru_map.cdl > t.txt
+      mv t.txt meta_gru_hru_map.cdl
 
-    #populate gru dimension with 1
-    ncap2 -h -s 'gru_ix(0)=1' ${example}_gru_hru_map.nc -O ${example}_gru_hru_map.nc
+      #make netcdf file quick
+      ncgen ./meta_gru_hru_map.cdl -o ${example}_gru_hru_map_${runtype}.nc
 
-    ncap2 -h -s 'hruCount(0)='$num_hru'' ${example}_gru_hru_map.nc -O ${example}_gru_hru_map.nc
+      #populate gru dimension with 1
+      ncap2 -h -s 'gru_ix(0)=1' ${example}_gru_hru_map_${runtype}.nc -O ${example}_gru_hru_map_${runtype}.nc
 
-    cnt=0
-    while read hru
-    do
-      ncap2 -h -s 'hru_id('${cnt}')='${hru}'' ${example}_gru_hru_map.nc -O ${example}_gru_hru_map.nc
-      ncap2 -h -s 'hru_ix('${cnt}')='$((${cnt}+1))'' ${example}_gru_hru_map.nc -O ${example}_gru_hru_map.nc
-      cnt=$(($cnt+1))
-    done < hru_list.txt
+      ncap2 -h -s 'hruCount(0)='$num_hru'' ${example}_gru_hru_map_${runtype}.nc -O ${example}_gru_hru_map_${runtype}.nc
+
+      cnt=0
+      while read hru
+      do
+	ncap2 -h -s 'hru_id('${cnt}')='${hru}'' ${example}_gru_hru_map_${runtype}.nc -O ${example}_gru_hru_map_${runtype}.nc
+	ncap2 -h -s 'hru_ix('${cnt}')='$((${cnt}+1))'' ${example}_gru_hru_map_${runtype}.nc -O ${example}_gru_hru_map_${runtype}.nc
+	cnt=$(($cnt+1))
+      done < hru_list.txt
+    done
   
     #clean up a bit
     rm hru_list.txt
